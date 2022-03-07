6cf0275e796964eaebeb237a03ac0a96e7a3cd1b
Jeremy Castillo <jeremyzetas@gmail.com>
2022-03-07 10:33:22 -0500

    working in attemps

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
-
+from gettext import find
 from Words import Words
+import os
 
 
 class Game:
@@ -9,9 +10,60 @@ class Game:
 
     def start(mode, diff):
 
+        os.system('clear')
+        
+
+
+        #Pick a random word or phrase 
         if mode == 1:
-            Words.words(diff)
+            print('\n\n Choosing a random word...')
+            pick = Words.words()
         elif mode == 2:
             pass #Phrases
         else:
-            pass #Catch errors
+            pass #Catch an error
+
+
+        #Amount of lives
+        if diff == 1:
+            lives = 10
+        elif diff == 2:
+            lives = 5
+        elif diff == 3:
+            lives = 3
+        else: 
+            pass #Catch an error
+
+
+        #Duplicate pick and replace each character by underscore
+        key = pick
+        mytable = key.maketrans("qwertyuiopasdfghjklzxcvbnm","__________________________")
+        guess = key.translate(mytable)
+        print(key)
+
+
+        #prompt for an attemp
+        while guess != key:
+
+            print(guess+"\n\n")
+            attemp = input('\U0001F449' + ' ')
+            if len(attemp) == 1:
+
+                for letter in key:
+                    print(letter)
+                    if letter == attemp:
+                        x = key.find(attemp)
+                        #guess[x] = attemp
+                        i = guess.replace(letter,attemp)
+                        guess = i
+                        print('founded')
+                    else:
+                        print('not found')          
+
+            else:
+                print('Try entering one character')
+
+
+
+
+
--- Words.py
+++ Words.py
@@ -1,22 +1,8 @@
-from multiprocessing.reduction import duplicate
 import random
-from turtle import st
 
 class Words:
 
-    def words(diff):
-
-
-        #Amount of lives
-        if diff == 1:
-            lives = 10
-        elif diff == 2:
-            lives = 5
-        elif diff == 3:
-            lives = 3
-        else: 
-            pass #Catch an error
-
+    def words():
 
         #Creating a list with words
         words = []
@@ -27,16 +13,6 @@ class Words:
         #Pick a random word of the list
         word = random.choice(words)
         word = word.replace(word[-1], '')
-        
-        #Convert that word in an array of characters "key" 
-        #key = []
-        #for word in word:
-        #    key.append(word)
 
-        #Duplicate that and replace each character by underscore
-        key = word
-        mytable = key.maketrans("qwertyuiopasdfghjklzxcvbnm","__________________________")
-        print(key.translate(mytable))
-      
-    def selection():
-        pass
+        return word
+
